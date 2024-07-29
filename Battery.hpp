1ebc5d53fd3b9a2179cbb31245fe5d17c915390b
sumanthsags <120707040+sumanthsags@users.noreply.github.com>
2024-07-29 21:29:07 +0530

    Create Battery.hpp

---
+++ Battery.hpp
@@ -0,0 +1,20 @@
+#ifndef BATTERY_HPP
+#define BATTERY_HPP
+
+#include <vector>
+#include "ParameterCheck.hpp"
+#include "TemperatureCheck.hpp"
+#include "SocCheck.hpp"
+#include "ChargeRateCheck.hpp"
+
+class Battery {
+    float temperature;
+    float soc;
+    float chargeRate;
+    std::vector<ParameterCheck*> checks;
+public:
+    Battery(float temp, float stateOfCharge, float charge, std::vector<ParameterCheck*> checkArray);
+    bool isBatteryOk(std::vector<std::string>& messages);
+};
+
+#endif // BATTERY_HPP
