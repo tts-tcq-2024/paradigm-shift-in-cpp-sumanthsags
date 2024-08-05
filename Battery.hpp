823e4c0989adc1bf0298dbe5bfd94553f26d06f8
sumanthsags <120707040+sumanthsags@users.noreply.github.com>
2024-08-05 15:02:29 +0530

    Update Battery.hpp

--- Battery.hpp
+++ Battery.hpp
@@ -6,7 +6,6 @@
 #include "TemperatureCheck.hpp"
 #include "SocCheck.hpp"
 #include "ChargeRateCheck.hpp"
-#include "BatteryCheck.hpp"
 #include <string>
 
 class Battery {
@@ -16,7 +15,6 @@ class Battery {
     std::vector<ParameterCheck*> checks;
 public:
     Battery(float temp, float stateOfCharge, float charge, std::vector<ParameterCheck*> checkArray);
-    bool checkBatteryParameter(std::vector<ParameterCheck*> check,double value, std::vector<std::string>& messages, int& warnings, int& breaches);
     bool isBatteryOk(std::vector<std::string>& messages);
 };
 
