6a641d537b7df5a0eff27ba03868515038690662
sumanthsags <120707040+sumanthsags@users.noreply.github.com>
2024-08-05 21:27:03 +0530

    Update SocCheck.hpp

--- SocCheck.hpp
+++ SocCheck.hpp
@@ -6,14 +6,17 @@
 #include <vector>
 #include <string>
 #include <unordered_map>
+#include <map>
 
 class SocCheck : public ParameterCheck {
-    std::vector<Range> ranges;
-    std::unordered_map<BatteryStatus, std::string> statusMessages;
 public:
     SocCheck();
     BatteryStatus check(float value) override;
     std::string message(BatteryStatus status) override;
+
+private:
+    std::map<float, BatteryStatus> socThresholds;
+    std::unordered_map<BatteryStatus, std::string> statusMessages;
 };
 
 #endif // SOC_CHECK_HPP
